6ac2e96ea7302ee799ba235501eea1fba8d6b946
Sarthak Bhardwaj <42564358+Sarthakbhardwaj@users.noreply.github.com>
2022-09-07 00:08:30 +0530

    Create Bloom Filters.py

---
+++ Bloom Filters.py
@@ -0,0 +1,6 @@
+Youtube link: https://www.youtube.com/watch?v=V3pzxngeLqw&list=WL&index=1
+
+1. They are used in NoSQL databases to quickly read data
+2. they mainly work on hashing
+3. they return 2 values either "False" and "maybe true"
+4. 
