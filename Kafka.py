641edc747c2a1b504fdfd00476c2628b1d647e31
Sarthak Bhardwaj <42564358+Sarthakbhardwaj@users.noreply.github.com>
2022-10-10 13:24:28 +0530

    Create Kafka.py

---
+++ Kafka.py
@@ -0,0 +1,17 @@
+1. fast, scalable, fault-tolerant, publish-subscribe messaging system
+2. highly available and resilient to node failures and supports automatic recovery
+3. Components of Kafka: a) broker b) producer c) consumer
+4. broker: is a node on kafka cluster which is used to persist and replicate the data. 
+5. producer: pushes the message into the message container called kafka topic
+6. consumer: pulls the message from the kafka topic
+
+
+MESSAGING SYSTEM IN KAFKA
+
+distributed messaging is based on reliable message queuing 
+messages are asynchronously queued between client applications and messaging systems. 
+
+there are 2 types of messaging patterns available;
+a) point to point
+b) publish-subscribe messaging system. 
+
