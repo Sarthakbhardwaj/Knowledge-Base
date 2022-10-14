bc4ee7f268ae11cd970d389f2b5023397c611044
Sarthak <sarthakbhardwaj274@gmail.com>
2022-10-14 10:58:34 +0530

    added more notes till common issues

--- FacebookAds.py
+++ FacebookAds.py
@@ -1,1 +1,22 @@
-1. hello bhai 
+# FACEBOOK ADS:
+
+# sampling looks at a portion of data that represents a larger population included in an entire set of data. metrices that count are sampled because it takse a lrage amount of data to caclulate them
+
+# there are three user access levels:
+# 1) admin 
+# 2) advertiser
+# 3) analyst
+
+# we need atleast analyst user access to view and access reports 
+
+
+# COMMON ISSUES:
+
+# DATA MISMTAHC/MISSING:
+# 1. mismatch between the user dashboard attribution settings and hevo click_attribution and view_attribution settings.
+# 1.a.) this can laso happen if user runs differnt attribution settings for different ad sets. there will be a mismatch due to different setting ad sets. 
+
+# 2. comparing different time and aggregation windows in the dashboard and hevo report. 
+# 2.a. discrepancies in aggregation window. user has to mathc their dashboard reporting times and hevo report date times for values to match 
+# 2.b if the user is checking latest numbers in their dashboard and hevo's last ingestion + refresh happened x minutes back 
+
